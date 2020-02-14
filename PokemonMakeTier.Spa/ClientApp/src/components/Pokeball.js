7c12792440df30873f982643b512a440d6e6f523
Dnniz <dannuzp@gmail.com>
2020-02-14 15:52:13 -0500

    Implementación de Pokeball MOVE!

--- PokemonMakeTier.Spa/ClientApp/src/components/Pokeball.js
+++ PokemonMakeTier.Spa/ClientApp/src/components/Pokeball.js
@@ -1,34 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles/Pokeball.css";
 import { keyframes } from "styled-components";
 import styled from "styled-components";
+import { Route } from "react-router-dom";
+
+function useMovePokeball(index) {
+  const [prevVector, setPrevVector] = useState({
+    x: 0,
+    y: 0
+  });
+
+  const [nextVector, setNextVector] = useState({
+    x: 0,
+    y: 0
+  });
+
+  useEffect(() => {
+    const idInterval = setTimeout(() => {
+      setPrevVector({ x: nextVector.x, y: nextVector.y });
+      setNextVector({
+        x: Math.random() * (index % 2 === 0 ? -1 * index : index),
+        y: Math.random() * (index % 2 === 0 ? -1 * index : index)
+      });
+    }, 8000);
+
+    //x: Math.random() * (index % 2 === 0 ? -1 * index : index) < 15? cantidad * Math.random() :  (index % 2 === 0 ? -1 * index : index
+    //y: Math.random() * (index % 2 === 0 ? -1 * index : index) < 15? cantidad * Math.random() :  (index % 2 === 0 ? -1 * index : index)
+
+    return () => clearTimeout(idInterval);
+  });
+
+  return [prevVector.x, prevVector.y, nextVector.x, nextVector.y];
+}
 
 function Pokeball(props) {
-  let to = {
-    x: Math.random() * (props.index % 2 === 0 ? -11 : 11),
-    y: Math.random() * 12
-  };
-  let spin = keyframes` from {
-    transform:  translate(0, 0);
-  }
+  const [prevX, prevY, nextX, nextY] = useMovePokeball(props.index);
 
-  to {
-    transform: translate(${to.x}rem, ${to.y}rem);
-  }
-`;
+  let move = keyframes`
+                    from {
+                      transform:  translate(${prevX}vw, ${prevY}vh);
+                    }
+                    to {
+                      transform: translate(${nextX}vw, ${nextY}vh);
+                    }
+                    `;
 
-  //${(Math.random() + 1) * 2000}s
+  // let move = keyframes`
+  //                   from {
+  //                     transform:  translate(${prevX}rem, ${prevY}rem);
+  //                   }
+  //                   to {
+  //                     transform: translate(${nextX}rem, ${nextY}rem);
+  //                   }
+  //                   `;
+
+  //   const Move = styled.div`
+  //   animation: ${move} 4s ease-in-out alternate both infinite;
+  // `;
 
   const Move = styled.div`
-    animation: ${spin} 2s ease-in-out alternate both infinite;
+    animation: ${move} 8s ease-out alternate both infinite;
   `;
-  //`${keyframes} 2s ease-in-out alternate both infinite`
-  // const Spin = {
-  //   animation: `${spin} 2s ease-in-out alternate both infinite`
-  // };
 
-  const rotate = keyframes`
+  return (
+    <Move>
+      <div className="ball poke">
+        <div className="pokeball-btn" onClick={props.onClickPokeball}></div>
+      </div>
+    </Move>
+  );
+}
+
+export default Pokeball;
+
+/*
+OTROS EJEMPLOS
+
+
+ const rotate = keyframes`
   from {
     transform: rotate(0deg);
   }
@@ -44,7 +94,8 @@ function Pokeball(props) {
     font-size: 1.2rem;
   `;
 
-  // handleAnimation = index => {
+
+   // handleAnimation = index => {
   //   let to = {
   //     x: Math.random() * (index % 2 === 0 ? -11 : 11),
   //     y: Math.random() * 12
@@ -62,19 +113,7 @@ function Pokeball(props) {
   //     easing: "ease-in-out"
   //   };
 
-  //   return { keyframes, options };
+  //   return keyframes, options;
   // };
 
-  return (
-    <Move>
-      <div
-        className="ball poke"
-        // animation={() => this.handleAnimation(this.props.onAnimate)}
-      >
-        <div className="pokeball-btn"></div>
-      </div>
-    </Move>
-  );
-}
-
-export default Pokeball;
+*/
--- PokemonMakeTier.Spa/ClientApp/src/pages/Home.js
+++ PokemonMakeTier.Spa/ClientApp/src/pages/Home.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 import "./styles/Home.css";
-// import "../components/styles/Pokeball.css";
-
 import Pokeball from "../components/Pokeball";
 
 class Home extends React.Component {
@@ -11,9 +9,13 @@ class Home extends React.Component {
 
     this.state = {
       pokeballs: [],
-      numPokeballs: 50
+      numPokeballs: 20
     };
   }
+
+  handleClickPokeball = dexId => {
+    alert("POKEMON!" + dexId);
+  };
 
   render() {
     if (this.state.pokeballs.length > 0) {
@@ -33,47 +35,20 @@ class Home extends React.Component {
     );
   }
 
-  // handleAnimation = index => {
-  //   let to = {
-  //     x: Math.random() * (index % 2 === 0 ? -11 : 11),
-  //     y: Math.random() * 12
-  //   };
-
-  //   const keyframes = [
-  //     { transform: "translate(0, 0)" },
-  //     { transform: `translate(${to.x}rem, ${to.y}rem)` }
-  //   ];
-  //   const options = {
-  //     duration: (Math.random() + 1) * 2000, // random duration
-  //     direction: "alternate",
-  //     fill: "both",
-  //     iterations: Infinity,
-  //     easing: "ease-in-out"
-  //   };
-
-  //   return keyframes, options;
-  // };
-
   componentDidMount() {
     this.createPokeballs();
   }
 
   createPokeballs = () => {
     for (let index = 1; index <= this.state.numPokeballs; index++) {
-      // const element = <Pokeball />;
-      // element.animate(() => this.handleAnimation(index));
-
-      // let ball = document.createElement("div");
-      // let button = ball.appendChild(document.createElement("div"));
-      // ball.classList.add("ball");
-      // ball.classList.add("poke");
-      // ball.setAttribute("id", { index });
-      // button.classList.add("pokeball-btn");
-
       this.setState(prevState => ({
-        // pokeballs: [...prevState.pokeballs, ball]
-        // pokeballs: [...prevState.pokeballs, element]
-        pokeballs: [...prevState.pokeballs, <Pokeball index={index} />]
+        pokeballs: [
+          ...prevState.pokeballs,
+          <Pokeball
+            index={index}
+            onClickPokeball={() => this.handleClickPokeball(index)}
+          />
+        ]
       }));
     }
   };
